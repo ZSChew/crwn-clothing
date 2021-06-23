c9f5311573586d89222603594d6dd06438db06c2
zhen shan <zhenshan.chew@gmail.com>
2021-06-23 15:39:53 +0800

    upload images

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        return (
+            <h1>Shop Page</h1>
+        );
+    }
+}
+
+export default ShopPage;
