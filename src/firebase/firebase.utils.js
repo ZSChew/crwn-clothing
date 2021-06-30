453158892107fe4004873b7751292e6657f2af84
zhen shan <zhenshan.chew@gmail.com>
2021-06-30 17:46:15 +0800

    WIP - storing user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,12 @@ const config = {
     appId: "1:696564579519:web:2e706a5c21fb5b573b9a0a"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    console.log(firestore.doc('users/b789ecg89we'));
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('RRneDTUME1Dr90YxE484');
+firestore.doc('/users/RRneDTUME1Dr90YxE484');
