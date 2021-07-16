ebaeac34e57a651dc751c6ff25ce293521c90e4e
zhenshan <zhenshan.chew@gmail.com>
2021-07-16 09:06:54 +0800

    install redux-persist and applied to the app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store';
+import { store, persistor } from "./redux/store";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
