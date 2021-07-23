9bbf58a6057e27f4dc3a7bdb5c0a149c643fdf66
zhenshan <zhenshan.chew@gmail.com>
2021-07-23 08:37:05 +0800

    remove sbkey

--- src/components/stripe-button/stripe-button.components.jsx
+++ src/components/stripe-button/stripe-button.components.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const publishableKey = "pk_test_51JFqBeFadC7TpMtJz8PLzeaapaqdFNpu2amCGAAjUuIkDbehYoBHXSlPtJMzD7k0j43yb1ShIK8z16X6Ig3Uj7Gp00A40C8Clz";
+    const publishableKey = "";
 
     const onToken = token => {
         console.log(token);
