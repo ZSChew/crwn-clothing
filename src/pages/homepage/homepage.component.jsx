06794f0d4f21b9dacb37f9c491d95bc480b7101c
zhenshan <zhenshan.chew@gmail.com>
2021-07-24 10:23:08 +0800

    added styled-component, apply styled-components to homepage and header

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,8 +4,12 @@ import Directory from '../../components/directory/directory.components';
 
 import './homepage.styles.scss';
 
+import { HomePageContainer } from './homepage.styles';
+
 const HomePage = () => (
-    <Directory />
+    <HomePageContainer>
+        <Directory />
+    </HomePageContainer>
 )
 
 export default HomePage;
