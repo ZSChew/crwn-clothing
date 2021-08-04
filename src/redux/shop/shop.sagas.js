712b3e0b2c6362c71e6c32c4a788abf53a46acaf
zhenshan <zhenshan.chew@gmail.com>
2021-08-04 09:14:05 +0800

    move redux thunk to sagas for shop

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,40 @@
+import { takeEvery, call, put } from "@redux-saga/core/effects";
+
+import {
+  firestore,
+  convertColletionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+  yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertColletionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+
+  // collectionRef
+  //   .get()
+  //   .then((snapshot) => {
+  //     const collectionsMap = convertColletionsSnapshotToMap(snapshot);
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //   })
+  //   .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
